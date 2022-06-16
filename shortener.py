408b8e906ea5dc679c17a1e6d8a54dec4e831382
Rahul Ramesh <60769128+rahulramesh96@users.noreply.github.com>
2022-06-16 22:43:56 +0100

    Update shortener.py

--- shortener.py
+++ shortener.py
@@ -11,7 +11,7 @@ except ImportError:
 import sys
 
 def make_tiny(url):
-  request_url = ('http://tinyurl.com/api.create.php?'+urlencode({url':url}))
+  request_url = ('http://tinyurl.com/api.create.php?'+urlencode({'url':url}))
   with contextlib.closing(urlopen(request_url)) as response:
   return response.read().decode('utf-8')
   
